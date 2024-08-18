df5acb0a211889613f2cda6a98a1e719cf9b1d77
unknown <minhajsulmuneeb@gmail.com>
2024-08-18 05:09:29 +0500

    fixing metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,6 +10,7 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "chatapp",
   description: "chatapp",
+  icons : '/favicon.ico',
 };
 
 export default function RootLayout({
